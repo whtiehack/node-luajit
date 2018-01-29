3a7f0ff8016559239f1639cacf808f2b83eb9feb
whitehack <whitehack@163.com>
2018-01-29 15:19:12 +0800

    add,linux auto compile luajit. add, cjson compile and auto load.

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ similar as `https://github.com/brettlangdon/NodeLua`
 2. nodejs -> lua  in queue, so lua internal is sync.
 3. all luaState  are independent.
 4. now lua can't call nodejs direct.
+5. add,linux auto compile luajit.[2018-01-29] http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
+6. add, cjson compile and auto load.  [2018-01-29] https://github.com/openresty/lua-cjson/archive/master.zip
+
 
 
 ##Installation
--- prepare.js
+++ prepare.js
@@ -2,7 +2,6 @@
 
 // prepare luajit cjson protobuf
 
-const shell = require('shelljs');
 // (test -d 3rdlibs || mkdir 3rdlibs) && cd 3rdlibs && wget http://luajit.org/download/LuaJIT-2.0.5.tar.gz && tar -xf LuaJIT-2.0.5.tar.gz
 
 const url = 'http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz';
@@ -10,43 +9,36 @@ const cjsonurl = 'https://github.com/openresty/lua-cjson/archive/master.zip';
 const cjsonname = 'lua-cjson';
 const luajitname = 'LuaJIT';
 const os = require('os');
-if(os.platform() === 'darwin'){
-//    console.error(new Error('auto prepare,maybe not support '+ os.platform()));
- //   return;
-    console.log('os x can not use luajit');
-    return;
-}
-if(os.platform()!=='linux'){
-    console.log('prepare only for linux');
-    return;
-}
-if(!shell.test('-d','3rdlibs')){
-    shell.mkdir('3rdlibs');
-}else{
-    shell.rm('-rf','3rdlibs/'+luajitname);
-    shell.rm('-rf','3rdlibs/'+cjsonname);
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+if(os.platform() === 'win32'){
+    //  shell.exec('sudo make install');
+    // console.warn(' windows maybe trigger error. because windows not have wget,tar and unzip command!');
 }
 
-shell.cd('3rdlibs');
-if(shell.test('-d',luajitname)){
-    console.log(luajitname+' has exists');
+if(os.platform() === 'darwin'){
+    console.warn('os x can not use luajit,use default maclualib');
     return 0;
 }
+let child;
 
-shell.exec('wget '+ url + ' -O '+luajitname+'.tar.gz');
-shell.exec('mkdir '+luajitname);
-shell.exec('tar -xf '+luajitname+'.tar.gz --strip-components 1 -C ./'+luajitname);
-shell.rm(luajitname+'.tar.gz');
-shell.cd(luajitname);
-shell.exec('make');
-shell.cd('..');
-shell.exec('wget ' + cjsonurl + ' -O '+ cjsonname +'.zip');
-shell.exec('unzip -d '+ cjsonname+ ' -j '+ cjsonname +'.zip');
-shell.rm(cjsonname+'.zip');
+if(os.platform() === 'win32'){
+    child = spawn('cd 3rdlibs\\LuaJIT\\src && msvcbuild.bat',{shell:true});
+}else{
+    child = spawn('cd 3rdlibs/LuaJIT && make && make install',{shell:true});
+}
+
+child.stdout.pipe(process.stdout);
+
+child.stderr.pipe(process.stderr);
+
+child.on('close',function(code){
+    console.log('compile LuaJIT result code:',code);
+});
+
+
 //shell.cd(cjsonname);
 //shell.exec('make LUA_INCLUDE_DIR="../'+luajitname+'/src" LUA_BIN_DIR="../'+ luajitname+'/src" LUA_CMODULE_DIR="../' + luajitname+'/src" LUA_MODULE_DIR="../'+luajitname+'/src"');
-if(os.platform() !== 'win32'){
-  //  shell.exec('sudo make install');
-}
+
 return 0;
 
